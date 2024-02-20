048ef9897f45869d2a60d7349db415b86c773715
allAboutManas <rm.manasraj@gmail.com>
2024-02-21 01:27:37 +0530

    js file changed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function red(){
     document.getElementsByClassName('stop')[0].style.backgroundColor = "red";
-    document.getElementsByClassName('wait')[0].style.backgroundColor = "rgb(242, 242, 148)";
-    document.getElementsByClassName('go')[0].style.backgroundColor = "rgb(140, 226, 140)";
+    document.getElementsByClassName('wait')[0].style.backgroundColor = "black";
+    document.getElementsByClassName('go')[0].style.backgroundColor = "blackgit";
  }
  function yellow(){
     document.getElementsByClassName('wait')[0].style.backgroundColor = "yellow";
-    document.getElementsByClassName('stop')[0].style.backgroundColor = "rgb(230, 118, 118)";
-    document.getElementsByClassName('go')[0].style.backgroundColor = "rgb(140, 226, 140)";
+    document.getElementsByClassName('stop')[0].style.backgroundColor = "black";
+    document.getElementsByClassName('go')[0].style.backgroundColor = "black";
  }
  function green(){
     document.getElementsByClassName('go')[0].style.backgroundColor = "green";
-    document.getElementsByClassName('wait')[0].style.backgroundColor = "rgb(242, 242, 148)";
-    document.getElementsByClassName('stop')[0].style.backgroundColor = "rgb(230, 118, 118)";
+    document.getElementsByClassName('wait')[0].style.backgroundColor = "black";
+    document.getElementsByClassName('stop')[0].style.backgroundColor = "black";
  }
